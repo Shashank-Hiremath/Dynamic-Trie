babf59b6b981b88452f42ff1112d7e5a1619a713
Shashank V Hiremath <shashankhire2@gmail.com>
2018-12-21 19:38:00 +0530

    Done hard delete

--- binary.c
+++ binary.c
@@ -3,37 +3,40 @@
 #include <math.h>
 short int trie[(1 << 15)][26], cnt[(1 << 15)];
 unsigned short int end;
+int deleteStack[1000], deleteTop, availableStack[100000], availableTop;
 
 short int nextAvailableNode()
 {
     //if stack is empty
-    //if(top==0)
-    end++;
-    return end;
+    if(availableTop==0)
+        return ++end;
+    return availableStack[--availableTop];
 }
 
 void insertTrie(char *str)
 {
-    //printf("%c\n",str[1]);
     int curr = 1, i;
     for (i = 0; str[i + 1]; i++)
         if (trie[curr][str[i] - '0'])
             curr = (trie[curr][str[i] - '0'] < 0) * -(trie[curr][str[i] - '0']) + (trie[curr][str[i] - '0'] >= 0) * trie[curr][str[i] - '0'];
         else
+        {
+            cnt[curr]++;
             curr = trie[curr][str[i] - '0'] = nextAvailableNode();
+        }
     if (trie[curr][str[i] - '0'] < 0)
         printf("%s already present\n", str);
     else if (trie[curr][str[i] - '0'] > 0)
-        {
-            trie[curr][str[i] - '0'] *= -1;
-            cnt[curr]++;
-            printf("%s is inserted\n", str);
-        }
+    {
+        trie[curr][str[i] - '0'] *= -1;
+        printf("%s is inserted\n", str);
+    }
     else
-        {
-            trie[curr][str[i] - '0'] = -nextAvailableNode();
-            printf("%s is inserted\n", str);
-        }
+    {
+        cnt[curr]++;
+        trie[curr][str[i] - '0'] = -nextAvailableNode();
+        printf("%s is inserted\n", str);
+    }
 }
 
 void searchTrie(char *str)
@@ -56,7 +59,12 @@ void searchTrie(char *str)
 void deleteTrie(char *str)
 {
     int curr = 1, i;
+    deleteTop = 0;
     for (i = 0; str[i + 1]; i++)
+    {
+
+        deleteStack[deleteTop++] = curr;
+
         if (trie[curr][str[i] - '0'])
             curr = (trie[curr][str[i] - '0'] < 0) * -(trie[curr][str[i] - '0']) + (trie[curr][str[i] - '0'] >= 0) * trie[curr][str[i] - '0'];
         else
@@ -64,9 +72,25 @@ void deleteTrie(char *str)
             printf("%s not present\n", str);
             return;
         }
+    }
     if (trie[curr][str[i] - '0'] < 0)
     {
-        trie[curr][str[i] - '0'] *= -1;
+        while (deleteTop>=0)
+        {
+            if (cnt[curr] > 1) //Remove only flag and retain node, for the sake of its other children
+            {
+                trie[curr][str[i] - '0'] *= -1;
+                break;
+            }
+            else
+            {
+                trie[curr][str[i] - '0'] = 0;
+                availableStack[availableTop++] = curr;
+                deleteTop--;
+                if(deleteTop)
+                    curr = deleteStack[deleteTop];
+            }
+        }
         printf("%s deleted\n", str);
     }
     else
@@ -79,17 +103,25 @@ int main()
     //printf("%d\n",sizeof(trie[0][0]));
     memset(trie, 0, sizeof(trie));
     memset(cnt, 0, sizeof(cnt));
+    availableTop=0;
     //length=atoi(fread(fp))
     end = 1;
     insertTrie("apple");
     insertTrie("apple");
-    insertTrie("apple");
+    insertTrie("ball");
     insertTrie("apple");
     searchTrie("apple");
+    searchTrie("ball");
     searchTrie("apple1");
     deleteTrie("apple");
+    printf("%d\n",availableTop);
+    deleteTrie("ball");
+    printf("%d\n",availableTop);
+    searchTrie("ball");
     deleteTrie("apple1");
+    insertTrie("apple");
     searchTrie("apple");
+    printf("%d\n",availableTop);
     //fwrite(fp,length);
     fclose(fp);
     return 0;
---
+++ test.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+short int trie[(1 << 15)][26], cnt[(1 << 15)];
+unsigned short int end;
+int deleteStack[1000],deleteTop,availableStack[100000],availableTop;
+
+
+int main()
+{
+     int a[3]={1,2,3},x=0;
+    printf("%d\n",a[++x]);
+    return 0;
+}
